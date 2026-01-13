98952f64a28f0ac7d785ec06318358cf9bd4f5d9
Ryan Luong <lumintriet@gmail.com>
2026-01-13 10:54:36 +0700

    feat: make fallback error message more robust.

--- src/utils/errors/fallback.ts
+++ src/utils/errors/fallback.ts
@@ -1,4 +1,6 @@
 import { UnexpectedError } from "@/utils/errors/classes";
+import { SUPPORTED_RUNTIMES } from "@/utils/constants";
+import { detectRuntime } from "@/utils/detect-runtime";
 
 export function createFallbackError({
   message,
@@ -9,20 +11,28 @@ export function createFallbackError({
   message?: string;
   error: unknown;
 }): UnexpectedError {
+  const runtime = detectRuntime();
+  const runtimeWarningMessage =
+    "An unexpected error occurred. This can happen because your runtime is not currently supported by web-courier. Please open an issue if you see this message.";
+  const shouldWarn = !SUPPORTED_RUNTIMES.includes(runtime);
+
+  const errorMessage =
+    message ?? (shouldWarn ? runtimeWarningMessage : undefined);
+
   if (error instanceof Error) {
-    return new UnexpectedError(message ?? error.message, {
+    return new UnexpectedError(errorMessage ?? error.message, {
       cause: error,
       context,
     });
   }
 
   try {
-    return new UnexpectedError(message ?? String(error), {
+    return new UnexpectedError(errorMessage ?? String(error), {
       cause: error,
       context,
     });
   } catch {
-    return new UnexpectedError(message ?? "Unknown error", {
+    return new UnexpectedError(errorMessage ?? "Unknown error", {
       cause: error,
       context,
     });
