061172446c1f3e5e064f3d9eba76f08eaf8d85d2
Ryan Luong <lumintriet@gmail.com>
2026-01-14 08:48:55 +0700

    refactor: check for http status code first.

--- src/get-response.ts
+++ src/get-response.ts
@@ -27,51 +27,51 @@ export async function getResponse({
   try {
     const request = createRequest(fetchInput, requestInit);
     const response = await webFetch(request);
+
+    if (!response.ok) {
+      const status = response.status;
+      if (status === 401 || status === 403) {
+        throw new UnauthorizedError({ response, request });
+      }
+
+      if (status === 404) {
+        throw new NotFoundError({ response, request });
+      }
+
+      if (status === 418) {
+        throw new ServerIsATeapotError({ response, request });
+      }
+
+      if (status === 429) {
+        throw new TooManyRequestsError({ response, request });
+      }
+
+      if (status >= 400 && status < 500) {
+        throw new ClientError({ response, request });
+      }
+
+      if (status >= 500) {
+        throw new ServerError({ response, request });
+      }
+
+      const fallbackError = new UnexpectedError("Unexpected HTTP status code", {
+        context: {
+          inputs: { responseBodyFormat, requestInit, fetchInput },
+          operation: "getResponse",
+          outputs: { response },
+        },
+      });
+      throw fallbackError;
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const body = await parseResponseBody({
       format: responseBodyFormat,
       response,
     });
 
-    if (response.ok) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return body;
-    }
-
-    const status = response.status;
-    if (status === 401 || status === 403) {
-      throw new UnauthorizedError({ response, request });
-    }
-
-    if (status === 404) {
-      throw new NotFoundError({ response, request });
-    }
-
-    if (status === 418) {
-      throw new ServerIsATeapotError({ response, request });
-    }
-
-    if (status === 429) {
-      throw new TooManyRequestsError({ response, request });
-    }
-
-    if (status >= 400 && status < 500) {
-      throw new ClientError({ response, request });
-    }
-
-    if (status >= 500) {
-      throw new ServerError({ response, request });
-    }
-
-    const fallbackError = new UnexpectedError("Unexpected HTTP status code", {
-      context: {
-        inputs: { responseBodyFormat, requestInit, fetchInput },
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        outputs: { parsedBody: body, response },
-        operation: "getResponse",
-      },
-    });
-    throw fallbackError;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return body;
   } catch (error) {
     if (
       error instanceof NetworkError ||
