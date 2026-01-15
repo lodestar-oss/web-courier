c5ef965375c5b180325421dbb4454db2f4ed5e65
Ryan Luong <lumintriet@gmail.com>
2026-01-15 09:15:59 +0700

    refactor: make branded type prop more unique.

--- src/types/brand.ts
+++ src/types/brand.ts
@@ -1,5 +1,5 @@
-declare const __brand: unique symbol;
+declare const __web_courier_brand: unique symbol;
 
 export type Branded<T, B> = Brand<B> & T;
 
-type Brand<B> = { [__brand]: B };
+type Brand<B> = { [__web_courier_brand]: B };
