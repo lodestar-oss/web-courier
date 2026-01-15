7952e25a2b6a80e48e3fd2b56dac9f632784e8af
Ryan Luong <lumintriet@gmail.com>
2026-01-15 09:10:52 +0700

    refactor: make error handling simpler in createRequest function.

--- src/create-request.ts
+++ src/create-request.ts
@@ -1,11 +1,21 @@
 import { createFallbackError } from "@/utils/errors/fallback";
-import { createURL } from "@/create-url";
+import { assertUrlString } from "@/utils/url-string";
 
 export function createRequest(input: RequestInfo | URL, init?: RequestInit) {
   try {
     const request = new Request(input, init);
     return request;
   } catch (error) {
+    if (error instanceof TypeError) {
+      // This should throw InvalidURLError
+      if (typeof input === "string") {
+        assertUrlString(input);
+      }
+      // If the above assertion doesn't throw,
+      // Then it's likely that the app is running in the browser
+      // and the URL contains credentials
+    }
+
     const fallbackError = createFallbackError({
       context: {
         url: input instanceof Request ? input.url : input.toString(),
@@ -13,25 +23,6 @@ export function createRequest(input: RequestInfo | URL, init?: RequestInit) {
       },
       error,
     });
-
-    if (error instanceof TypeError) {
-      // This case should not happen.
-      // This is just for narrowing the type of input
-      if (input instanceof Request) {
-        throw fallbackError;
-      }
-
-      // It is likely that the input is not a valid url
-      // Call createURL to throw the InvalidURLError
-      const url = createURL(input);
-      // If it doesn't throw, then we fallback to unexpected error
-      // This case will happen in the browser environment if the url contains credentials
-      // However, currently, we don't support browser yet so we fallback to unexpected error
-      if (url) {
-        throw fallbackError;
-      }
-    }
-
     throw fallbackError;
   }
 }
