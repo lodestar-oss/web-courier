ca14163d6f6d33551dad94d5030305cca24c85ef
Ryan Luong <lumintriet@gmail.com>
2026-01-16 09:42:21 +0700

    feat: catch user's defined abort reason.

--- src/web-fetch.ts
+++ src/web-fetch.ts
@@ -40,6 +40,15 @@ export async function webFetch(input: RequestInfo | URL, init?: RequestInit) {
       }
     }
 
+    if (init?.signal?.aborted) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const abortReason = init.signal.reason;
+      throw new AbortError(
+        typeof abortReason === "string" ? abortReason : "Request was aborted",
+        { cause: error }
+      );
+    }
+
     const fallbackError = createFallbackError({
       context: {
         url: input instanceof Request ? input.url : input.toString(),
