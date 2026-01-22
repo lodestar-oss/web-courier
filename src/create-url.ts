c2f47a4b8c53f3c27e3dec0f49d8c1b6f909f6c1
Ryan Luong <lumintriet@gmail.com>
2026-01-22 19:32:22 +0700

    feat: apply the result type.

--- src/create-url.ts
+++ src/create-url.ts
@@ -1,9 +1,14 @@
+import type { Result } from "@/types/result";
+
 import { WebCourierError } from "@/utils/errors/classes";
 
-export function createURL(url: string | URL, base?: string | URL) {
+export function createURL(
+  url: string | URL,
+  base?: string | URL
+): Result<URL, WebCourierError> {
   try {
     const urlObj = new URL(url, base);
-    return { success: true, url: urlObj };
+    return { success: true, data: urlObj };
   } catch (error) {
     if (error instanceof TypeError) {
       const invalidURLError = new WebCourierError("Invalid URL", {
